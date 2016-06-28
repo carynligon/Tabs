f9fdd98a9a2fa652b6cc5e84add385bd6c157c5b
Caryn Ligon <carynligon@gmail.com>
2016-06-28 12:45:15 -0500

    initial commit, some progress with js

---
+++ main.js
@@ -0,0 +1,22 @@
+var colors = {
+    red: {
+      title: 'Red',
+      description: 'RED Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    green: {
+      title: 'Green',
+      description: 'GREEN Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+    },
+    blue: {
+      title: 'Blue',
+      description: 'BLUE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+    }
+  };
+}
+
+window.addEventListener('hashchange', contentToChange)
+
+function renderContent (contentToChange) {
+  document.querySelector('#content h1').textContent = colors[contentToChange].title;
+  document.querySelector('#content main').textContent = colors[contentToChange].description;
+}
